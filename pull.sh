b10722e1804388f0c8ffb418dc89233199a36f1d
EngrUsmanPervez <38681527+EngrUsmanPervez@users.noreply.github.com>
2022-10-20 21:56:24 +0500

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,14 +1,16 @@
-#git config credential.helper store
-#git config --global user.email "usman.pervez@seecs.edu.pk"
-#git config --global user.name "EngrUsmanPervez"
-#git add .
-#git branch -M main
-#git push -u origin main
 #git clone https://github.com/EngrUsmanPervez/eupvim.git
-#EngrUsmanPervez
-#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
+#git config credential.helper store
 git reset --hard HEAD
 git pull https://github.com/EngrUsmanPervez/eupvim.git
 chmod 777 pull.sh
 cp -R .ctags.d ~/
 cp vimrc ~/.vimrc
+
+
+#git config --global user.email "usman.pervez@seecs.edu.pk"
+#git config --global user.name "EngrUsmanPervez"
+#git add .
+#git branch -M main
+#git push -u origin main
+#EngrUsmanPervez
+#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
