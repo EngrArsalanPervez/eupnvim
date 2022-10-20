491059155121e0f7ff52d2109530cd51739d002c
EngrUsmanPervez <usman.pervez@seecs.edu.pk>
2022-10-20 21:04:39 +0500

    second commit

---
+++ pull.sh
@@ -0,0 +1,6 @@
+#git config credential.helper store
+#EngrUsmanPervez
+#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
+#git pull https://github.com/ultratelecom/ultraport
+#git clone https://github.com/EngrUsmanPervez/eupvim.git
+
