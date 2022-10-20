1f18b504d356c1f8ee94a60734eaa8737b51b21b
EngrUsmanPervez <38681527+EngrUsmanPervez@users.noreply.github.com>
2022-10-20 21:58:59 +0500

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,4 +1,6 @@
 #git clone https://github.com/EngrUsmanPervez/eupvim.git
+#EngrUsmanPervez
+#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
 #git config credential.helper store
 git reset --hard HEAD
 git pull https://github.com/EngrUsmanPervez/eupvim.git
@@ -12,5 +14,3 @@ cp vimrc ~/.vimrc
 #git add .
 #git branch -M main
 #git push -u origin main
-#EngrUsmanPervez
-#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
