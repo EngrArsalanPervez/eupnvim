9f06c0d1c62033469a8799b317fdc05a4b87041a
EngrUsmanPervez <38681527+EngrUsmanPervez@users.noreply.github.com>
2022-10-20 21:07:14 +0500

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,6 +1,9 @@
 #git config credential.helper store
+#git config --global user.email "usman.pervez@seecs.edu.pk"
+#git config --global user.name "EngrUsmanPervez"
+#git branch -M main
+#git push -u origin main
+#git clone https://github.com/EngrUsmanPervez/eupvim.git
 #EngrUsmanPervez
 #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
-#git pull https://github.com/ultratelecom/ultraport
-#git clone https://github.com/EngrUsmanPervez/eupvim.git
-
+git pull https://github.com/EngrUsmanPervez/eupvim.git
