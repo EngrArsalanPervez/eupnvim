905a394ec19636dd68aeff96486034c19f3f1b87
EngrUsmanPervez <38681527+EngrUsmanPervez@users.noreply.github.com>
2023-05-17 16:23:41 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,30 @@
 # Pre Requisite
-apt install vim-gtk xclip ripgrep curl universal-ctags build-essential cmake vim-nox python3-dev
+apt update
+apt install vim-gtk xclip ripgrep curl universal-ctags build-essential cmake vim-nox python3-dev fuse libfuse2 git python3-pip ack-grep -y
 apt install mono-complete golang nodejs default-jdk npm
 autocmd FileType apache setlocal commentstring=#\ %s
 pip3 install pynvim
 
+# NeoVim Nightly Version
+wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
+chmod +x nvim
+sudo chown root:root nvim
+sudo mv nvim /usr/bin
+cd ~
+mkdir -p .config/nvim
+
+# Vim-Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Plugins
 cp -R .ctags.d ~/
 cp init.vim ~/config/nvim/init.vim
 
-#Install NerdFont
-#  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hermit.zip
-#  unzip Hermit.zip
-#  cp Hurmit*.otf /usr/share/fonts/
-#  cp Hurmit*.otf /usr/local/share/fonts/
+# Fonts
+cd ~
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hermit.zip
+unzip Hermit.zip
+cp Hurmit*.otf /usr/share/fonts/
+cp Hurmit*.otf /usr/local/share/fonts/
+
+#CommicShanns
