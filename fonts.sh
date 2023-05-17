106ffdb7dcad718f9482a85bffaf0e4c8844b604
EngrUsmanPervez <38681527+EngrUsmanPervez@users.noreply.github.com>
2023-05-17 16:54:38 +0500

    Update fonts.sh

--- fonts.sh
+++ fonts.sh
@@ -2,7 +2,7 @@ cd ~
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/ComicShannsMono.zip
 unzip ComicShannsMono.zip
 cp Comic*.otf /usr/share/fonts/
-cp Comic*.otf /usr/local/share/fonts/**
+cp Comic*.otf /usr/local/share/fonts/
 # Change fonts from Terminal->Edit->Preferences->Font
 
 
