2673542d8ef4abdd79848a49399ba59ccd517214
EngrUsmanPervez <38681527+EngrUsmanPervez@users.noreply.github.com>
2023-09-13 11:00:03 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 # Pre Requisite
-apt update
-apt install xclip ripgrep curl universal-ctags build-essential cmake vim-nox python3-dev fuse libfuse2 git python3-pip ack fd-find vim-gtk3 -y
-apt install mono-complete golang nodejs default-jdk npm -y
-apt install python3-pynvim
+sudo apt update
+sudo apt install xclip ripgrep curl universal-ctags build-essential cmake vim-nox python3-dev fuse libfuse2 git python3-pip ack fd-find vim-gtk3 -y
+sudo apt install mono-complete golang nodejs default-jdk npm -y
+sudo apt install python3-pynvim
 
 #ln -s $(which fdfind) ~/.local/bin/fd
 #export PATH="/usr/bin:$PATH"
-ln -s /usr/bin/fdfind /usr/bin/fd
+sudo ln -s /usr/bin/fdfind /usr/bin/fd
 
 # NeoVim Nightly Version
 wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
