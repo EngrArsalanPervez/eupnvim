d84b615741d7bf0fc2ddc99d1d5a48270ad5e045
Arsalan Pervez <usman.pervez@seecs.edu.pk>
2024-06-14 11:43:09 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,9 @@
 sudo apt update
 sudo apt install xclip ripgrep curl universal-ctags build-essential cmake python3-dev git python3-pip ack fd-find clang-format -y 
 sudo apt install python3-pynvim -y
-# YouCompleteMe
-#sudo apt install mono-complete golang nodejs default-jdk npm -y
 
-#ln -s $(which fdfind) ~/.local/bin/fd
-#export PATH="/usr/bin:$PATH"
 sudo ln -s /usr/bin/fdfind /usr/bin/fd
+fd
 
 # NeoVim Nightly Version
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
@@ -25,9 +22,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 sudo curl -fLo /root/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Plugins
-#cd ~/eupnvim/
-#cp -R .ctags.d ~/
-#sudo cp -R .ctags.d /root/
+cd ~/eupnvim/
 cp init.vim ~/.config/nvim/init.vim
 sudo cp init.vim /root/.config/nvim/init.vim
 
@@ -39,10 +34,6 @@ sudo cp Comic*.otf /usr/share/fonts/
 sudo cp Comic*.otf /usr/local/share/fonts/
 rm *.otf
 rm ComicShannsMono.zip
-
-#sudo apt -y update
-#sudo apt -y install nautilus ubuntu-desktop sshfs
-#sudo apt -y upgrade
 
 Green='\033[0;32m'
 NC='\033[0m' # No Color
